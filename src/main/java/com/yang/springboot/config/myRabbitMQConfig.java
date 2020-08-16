78c40cca4cec9ec7db35340e4d7551f1ab2bf07b
yang <2975165963@qq.com>
2020-08-16 14:04:49 +0800

    16 commit

--- src/main/java/com/yang/springboot/config/myRabbitMQConfig.java
+++ src/main/java/com/yang/springboot/config/myRabbitMQConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Configuration
 public class myRabbitMQConfig {
     @Autowired
